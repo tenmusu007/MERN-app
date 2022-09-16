988c8804b20224e2c0a5779a8a62ec8d397a7854
Atsuya Tanaka <tenmusu007@gmail.com>
2022-09-15 21:23:54 -0700

    hash psw

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -3,13 +3,15 @@ const bcrypt = require("bcrypt");
 
 const AuthRegisterPost = async (req, res) => {
 	try {
-		await bcrypt.hash(req.body.password, 12).then((hashedPassword) => {
-			console.log("hashed", hashedPassword);
-		});
+		const hashedPsw =await bcrypt
+			.hash(req.body.password, 12)
+			.then((hashedPassword) => {
+				return hashedPassword;
+			});
 		const newUser = await new User({
 			username: req.body.username,
 			email: req.body.email,
-			password: req.body.password,
+			password: hashedPsw,
 		});
 		const user = await newUser.save();
 		return res.status(200).json(user);
@@ -25,14 +27,20 @@ const AuthRegisterGet = (req, res) => {
 };
 const AuthLoginPost = async (req, res) => {
 	try {
-		req.session.name = req.body.username;
+		const hashedPsw = await bcrypt
+			.hash(req.body.password, 12)
+			.then((hashedPassword) => {
+				return hashedPassword;
+      });
+    console.log(hashedPsw);
 		const user = await User.findOne({ username: req.body.username });
 		if (!user) return res.status(404).send("We can not find the user");
 		const vailedPassword = req.body.password === user.password;
-		if (!vailedPassword) return res.status(400).json("password is wrong");
-		res.cookie("user", `${req.body.username}`);
-		// res.redirect("/timeline");
-		return res.status(200).json(user);
+		const isMatch = bcrypt.compare(user.password, hashedPsw);
+		if (!vailedPassword && isMatch) return res.status(400).json("password is wrong");
+		req.session.name = req.body.username;
+		// // res.redirect("/timeline");
+		// return res.status(200).json(user);
 	} catch (error) {
 		return res.status(500).json;
 	}
